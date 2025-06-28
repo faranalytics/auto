2e938c01a59f50ff8c3c6cfc68ee51df9c7fd7d3
Adam Patterson <adam@farar.net>
2025-06-28 16:37:54 -0400

    more

--- src/auto/__main__.py
+++ src/auto/__main__.py
@@ -35,7 +35,7 @@ INIT_SYSTEM_PROMPT_PATH = Path(config["DEFAULT"]["INIT_SYSTEM_PROMPT_PATH"])
 MODEL = config["DEFAULT"]["MODEL"]
 MODEL_MAX_TOKEN_COUNT = int(config["DEFAULT"]["MODEL_MAX_TOKEN_COUNT"])
 OPENAI_API_KEY = config["DEFAULT"]["OPENAI_API_KEY"]
-USER_PROMPT = config["DEFAULT"]["USER_PROMPT"]
+DEFAULT_USER_PROMPT = config["DEFAULT"]["DEFAULT_USER_PROMPT"]
 
 memory_path = STORE_PATH.joinpath("memory.json")
 transcript_path = STORE_PATH.joinpath("transcript.log")
@@ -64,36 +64,48 @@ if not memory_path.exists():
 while True:
 
     with open(memory_path, "r") as f:
-        context_window: List[Dict[str, str]] = json.load(f)
+        messages: List[Dict[str, str]] = json.load(f)
 
     print("\nBegin Memory\n")
-    print(context_window)
-    print("\nEnd Memory\n")
-    input("Press Enter to continue.")
+    pprint.pprint(messages)
+    print("\nEnd Memory")
 
-    print("\nQuerying API\n")
+    print("Querying API\n")
     client = OpenAI(api_key=OPENAI_API_KEY)
     completion = client.chat.completions.create(
         model=MODEL,
-        messages=context_window,
+        messages=messages,
         temperature=0,
     )
     content = completion.choices[0].message.content
 
-    print("\nBegin Content\n")
+    print("\nBegin Content")
     print(content)
-    print("\nEnd Content\n")
-    input("Press Enter to continue.")
+    print("End Content\n")
 
     content_soup = bs4.BeautifulSoup(content, "html.parser")
     updates: bs4.ResultSet[bs4.Tag] = content_soup.find_all(name="update")
     for update in updates:
-        print("Update: ", update)
         update_string = update.get_text().strip()
         update_id = update.attrs.get("id")
-        input("Press Enter to continue.")
-    context_window.append({"role": "assistant", "content": prepend_metadata(content=content)})
-    context_window.append({"role": "user", "content": prepend_metadata(content=USER_PROMPT)})
+        for index, message in enumerate(messages):
+            soup = bs4.BeautifulSoup(message["content"], "html.parser")
+            tag = soup.find(name="metadata", attrs={"id": update_id})
+            if tag:
+                messages[index]["content"] = str(tag) + update_string
+    deletes: bs4.ResultSet[bs4.Tag] = content_soup.find_all(name="delete")
+    for delete in deletes:
+        delete_id = delete.attrs.get("id")
+        for index, message in enumerate(messages):
+            soup = bs4.BeautifulSoup(message["content"], "html.parser")
+            tag = soup.find(name="metadata", attrs={"id": delete_id})
+            if tag:
+                del messages[index]
+    messages.append({"role": "assistant", "content": content})
+    user_prompt = input(f"""Enter a user message or press Enter to use the default user message: "{DEFAULT_USER_PROMPT}"\n> """)
+    if user_prompt == "":
+        messages.append({"role": "user", "content": prepend_metadata(content=DEFAULT_USER_PROMPT)})
+    else:
+        messages.append({"role": "user", "content": prepend_metadata(content=user_prompt)})
     with open(memory_path, "w") as f:
-        json.dump(context_window, f)
-    input("Press Enter to continue.")
+        json.dump(messages, f)
